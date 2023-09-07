4d35cbf9d480de5c513bd81d722c1b6ff2b7ec57
Luis PM <plpm90_nath@hotmail.com>
2023-09-07 05:51:23 -0600

    register email sending

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,6 +68,68 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     { 
+
+        $mailToClient = new PHPMailer(true);     // Passing `true` enables exceptions
+        
+        //Envio de notificación por correo al cliente
+        $emailRecipientClient = $data['email'];
+        $emailSubjectClient = 'Registro de Usuario - RTElSalvador';
+        $emailBodyClient = " 
+                        <div style='display:flex;justify-content:center;' >
+                            <img alt='rt-Logo' src='https://rtelsalvador.com/assets/img/rtthompson-logo.png' style='width:100%; max-width:250px;'>
+                        </div>
+
+                        <br/>
+                        <br/>
+
+                        <p><b>¡BIENVENIDO!</b></p>
+                        
+                        <br/>
+
+                        <p><b>Sr./Sra.</b>: ".$data['name']." </p>
+                        <p><b>Correo electrónico</b>: ".$data['email']."</p>
+
+                        <br/>
+                        
+                        <p>Porfavor completa el proceso de registro, llenando el formulario con tus datos.</p>
+                        ";
+                        
+        $replyToEmailClient = "oficina@rtelsalvador.com";
+        $replyToNameClient = "Representaciones Thompson";
+
+        $estado1 = $this->sendMail($mailToClient, $emailRecipientClient ,$emailSubjectClient ,$emailBodyClient ,$replyToEmailClient ,$replyToNameClient);
+
+
+        $mailToOffice = new PHPMailer(true);     // Passing `true` enables exceptions
+
+        //Envio de notificación por correo al cliente
+        $emailRecipientOff = $request->get('emailC');
+        $emailSubjectOff = 'Registro de Usuario - RTElSalvador';
+        $emailBodyOff = " 
+                        <div style='display:flex;justify-content:center;' >
+                            <img alt='rt-Logo' src='https://rtelsalvador.com/assets/img/rtthompson-logo.png' style='width:100%; max-width:250px;'>
+                        </div>
+
+                        <br/>
+                        <br/>
+
+                        <p><b>¡BIENVENIDO!</b></p>
+                        
+                        <br/>
+
+                        <p><b>Sr./Sra.</b>: ".$data['name']." </p>
+                        <p><b>Correo electrónico</b>: ".$data['email']."</p>
+
+                        <br/>
+                        
+                        <p>Porfavor completa el proceso de registro, llenando el formulario con tus datos.</p>
+                        ";
+                        
+        $replyToEmailOff = $data['email'];
+        $replyToNameOff = $data['name'];
+
+        $estado2 = $this->sendMail($mailToOffice, $emailRecipientOff ,$emailSubjectOff ,$emailBodyOff ,$replyToEmailOff ,$replyToNameOff);
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
@@ -79,4 +145,89 @@ class RegisterController extends Controller
             'boletin' => 1
         ]);
     }
+
+
+private function sendMail(PHPMailer $mail, $emailRecipient ,$emailSubject ,$emailBody ,$replyToEmail ,$replyToName ) 
+    {
+
+        require base_path("vendor/autoload.php");
+
+        //$mail = new PHPMailer(true);     // Passing `true` enables exceptions
+
+        try {
+
+            // Email server settings
+            $mail->SMTPDebug = 3;
+            $mail->isSMTP();
+            $mail->Host = env('SMTP_HOST', "");             //  smtp host p3plmcpnl492651.prod.phx3.secureserver.ne
+            $mail->SMTPAuth = true;
+            $mail->Username = env('SMTP_USERNAME', "");   //  sender username
+            $mail->Password = env('SMTP_PASS', "");       // sender password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                  // encryption - ssl/tls
+            $mail->Port = env('SMTP_PORT', "");                          // port - 587/465
+            $mail->SMTPKeepAlive = true;
+            $mail->CharSet = 'UTF-8';
+            $mail->Encoding = 'base64';
+
+            $mail->setFrom('notificaciones@rtelsalvador.com', 'Representaciones Thompson');
+            $mail->addAddress($emailRecipient); /* NOTA: mandar a llamar email según config en la BD*/
+            //$mail->addCC($request->emailCc);
+            //$mail->addBCC($request->emailBcc);
+
+            $mail->addReplyTo($replyToEmail, $replyToName);
+
+            /*
+            if(isset($_FILES['emailAttachments'])) {
+                for ($i=0; $i < count($_FILES['emailAttachments']['tmp_name']); $i++) {
+                    $mail->addAttachment($_FILES['emailAttachments']['tmp_name'][$i], $_FILES['emailAttachments']['name'][$i]);
+                }
+            }
+            */
+
+            $mail->isHTML(true);                // Set email content format to HTML
+
+            $mail->Subject = $emailSubject;
+            $mail->Body    = $emailBody;
+
+            // $mail->AltBody = plain text version of email body;
+
+            /* Se envia el mensaje, si no ha habido problemas la variable $exito tendra el valor true */
+            $exito = $mail->Send();
+            /* 
+            Si el mensaje no ha podido ser enviado se realizaran 4 intentos mas como mucho para intentar 
+            enviar el mensaje, cada intento se hara 5 segundos despues del anterior, para ello se usa la 
+            funcion sleep
+            */  
+            $intentos=1; 
+            
+            while ((!$exito) && ($intentos < 5)) {
+                sleep(5);
+                /*echo $mail->ErrorInfo;*/
+                $exito = $mail->Send();
+                $intentos=$intentos+1;  
+            }
+
+            $mail->getSMTPInstance()->reset();
+            $mail->clearAddresses();
+            $mail->smtpClose();
+
+            return $exito;
+        
+        } catch (Exception $e) {
+             return redirect()->route('inicio')->with('error','Ha ocurrido algún error al enviar.');
+        } 
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
