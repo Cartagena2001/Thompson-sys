9722f254103612eab4341864a95eac99446104b6
Luis PM <plpm90_nath@hotmail.com>
2024-01-05 14:13:42 -0600

    fix logActions middleware

--- app/Http/Middleware/LogActions.php
+++ app/Http/Middleware/LogActions.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Models\bitacora;
+use App\Models\Bitacora;
 use Illuminate\Support\Facades\Route;
 
 class LogActions
