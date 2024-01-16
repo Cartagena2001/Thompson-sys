0fd66799832ceb8368cc915aa7f411829cb8d6f9
Luis PM <plpm90_nath@hotmail.com>
2024-01-15 21:12:50 -0600

    quick fix 5

--- app/Http/Controllers/OrdenesController.php
+++ app/Http/Controllers/OrdenesController.php
@@ -318,7 +318,7 @@ class OrdenesController extends Controller
             $mail->SMTPSecure = env('MAIL_ENCRYPTION');    // encryption - ssl/tls
             //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // encryption - ssl/tls
             $mail->Port = env('MAIL_PORT');           // port - 587/465
-            //$mail->SMTPKeepAlive = true;
+            $mail->SMTPKeepAlive = true;
             $mail->CharSet = 'UTF-8';
             $mail->Encoding = 'base64';
 
@@ -339,7 +339,7 @@ class OrdenesController extends Controller
 
             $mail->isHTML(true);                // Set email content format to HTML
 
-            $mail->Subject = $emailSubject;
+            $mail->Subject = $emailSubject.' '.rand();
             $mail->Body    = $emailBody;
 
             // $mail->AltBody = plain text version of email body;
@@ -365,7 +365,7 @@ class OrdenesController extends Controller
 
             $mail->getSMTPInstance()->reset();
             $mail->clearAddresses();
-            //$mail->smtpClose();
+            $mail->smtpClose();
 
             return $mail->Send();
         
@@ -395,7 +395,7 @@ class OrdenesController extends Controller
             $mail->SMTPSecure = env('MAIL_ENCRYPTION');    // encryption - ssl/tls
             //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // encryption - ssl/tls
             $mail->Port = env('MAIL_PORT');           // port - 587/465
-            //$mail->SMTPKeepAlive = true;
+            $mail->SMTPKeepAlive = true;
             $mail->CharSet = 'UTF-8';
             $mail->Encoding = 'base64';
 
@@ -416,7 +416,7 @@ class OrdenesController extends Controller
 
             $mail->isHTML(true);                // Set email content format to HTML
 
-            $mail->Subject = $emailSubject;
+            $mail->Subject = $emailSubject.' '.rand();
             $mail->Body    = $emailBody;
 
             // $mail->AltBody = plain text version of email body;
@@ -442,7 +442,7 @@ class OrdenesController extends Controller
 
             $mail->getSMTPInstance()->reset();
             $mail->clearAddresses();
-            //$mail->smtpClose();
+            $mail->smtpClose();
 
             return $mail->Send();
         
